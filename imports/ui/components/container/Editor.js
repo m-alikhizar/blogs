864cea2cf716fedfc42457b0833f98ebd92b5668
Khizar Ali <khizar.ali.manzoor@gmail.com>
2019-06-17 04:27:13 +0500

    added quill editor configured while in meteor.isClient tu surpass any server side wrong errors.

--- imports/ui/components/container/Editor.js
+++ imports/ui/components/container/Editor.js
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {array, func} from "prop-types";
 
-const Editor = (props) => {
+class Editor extends Component {
+    constructor(props) {
+        super(props)
 
-    return (
-        <section className="featured-posts">
-            <div className="section-title">
-                <h2><span>Editor</span></h2>
-            </div>
-        </section>
-    );
+        if(Meteor.isClient){
+            this.quill = require('react-quill')
+        }
+
+        this.state = { text: '' }
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleChange(value) {
+        this.setState({ text: value })
+    }
+
+    render() {
+        if(this.quill) {
+            return (
+                <section className="featured-posts">
+                    <div className="section-title">
+                        <h2><span>Editor</span></h2>
+                        <this.quill value={this.state.text} onChange={this.handleChange} />
+                    </div>
+                </section>
+            )
+        } else {
+            return <div>No editor supported.</div>
+        }
+    }
 }
 
 Editor.propTypes = {
