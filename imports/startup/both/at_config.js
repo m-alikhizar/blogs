e0dd0e50173306a6ad888093c40bc3d74489e4be
Khizar Ali <khizar.ali.manzoor@gmail.com>
2019-06-25 05:48:51 +0500

    editor config

--- imports/startup/both/at_config.js
+++ imports/startup/both/at_config.js
@@ -40,18 +40,78 @@ AccountsTemplates.configure({
     postSignUpHook: myPostSubmitFunc,
 
     // Texts
+
     texts: {
         button: {
-            signUp: "Register Now!"
+            changePwd: "Password Text",
+            enrollAccount: "Enroll Text",
+            forgotPwd: "Forgot Pwd Text",
+            resetPwd: "Reset Pwd Text",
+            signIn: "Sign In Text",
+            signUp: "Sign Up Text",
         },
         socialSignUp: "Register",
         socialIcons: {
             "meteor-developer": "fa fa-rocket"
         },
         title: {
-            forgotPwd: "Recover Your Password"
+            changePwd: "Password Title",
+            enrollAccount: "Enroll Title",
+            forgotPwd: "Forgot Pwd Title",
+            resetPwd: "Reset Pwd Title",
+            signIn: "Sign In Title",
+            signUp: "Sign Up Title",
+            verifyEmail: "Verify Email Title",
         },
-    },
+
+
+
+        navSignIn: "signIn",
+        navSignOut: "signOut",
+        optionalField: "optional",
+        pwdLink_pre: "",
+        pwdLink_link: "forgotPassword",
+        pwdLink_suff: "",
+        resendVerificationEmailLink_pre: "Verification email lost?",
+        resendVerificationEmailLink_link: "Send again",
+        resendVerificationEmailLink_suff: "",
+        sep: "OR",
+        signInLink_pre: "ifYouAlreadyHaveAnAccount",
+        signInLink_link: "signin",
+        signInLink_suff: "",
+        signUpLink_pre: "dontHaveAnAccount",
+        signUpLink_link: "signUp",
+        signUpLink_suff: "",
+        socialAdd: "add",
+        socialConfigure: "configure",
+        socialIcons: {
+            // google: "myGoogleIcon",
+            // "meteor-developer": "myMeteorIcon",
+        },
+        inputIcons: {
+            isValidating: "fa fa-spinner fa-spin",
+            hasSuccess: "fa fa-check",
+            hasError: "fa fa-times",
+        },
+        errors: {
+            accountsCreationDisabled: "Client side accounts creation is disabled!!!",
+            cannotRemoveService: "Cannot remove the only active service!",
+            captchaVerification: "Captcha verification failed!",
+            loginForbidden: "error.accounts.Login forbidden",
+            mustBeLoggedIn: "error.accounts.Must be logged in",
+            pwdMismatch: "error.pwdsDontMatch",
+            validationErrors: "Validation Errors",
+            verifyEmailFirst: "Please verify your email first. Check the email and follow the link!",
+        },
+        socialRemove: "remove",
+        socialSignIn: "signIn",
+        socialSignUp: "signUp",
+        socialWith: "with",
+        termsPreamble: "clickAgree",
+        termsPrivacy: "privacyPolicy",
+        termsAnd: "and",
+        termsTerms: "terms",
+    }
 });
 
 
