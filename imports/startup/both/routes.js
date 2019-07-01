466fe05f69bf85c120df94ee0d813d1b08fb52fa
Khizar Ali <khizar.ali.manzoor@gmail.com>
2019-07-02 04:26:14 +0500

    accounts management and modal before hooking with actual api calls.

--- imports/startup/both/routes.js
+++ imports/startup/both/routes.js
@@ -4,15 +4,15 @@ import Main from '../../ui/layouts/main/Main';
 import Secondary from '../../ui/layouts/secondary/Secondary'
 
 import Home from '../../ui/pages/home/Home';
-import Subpage from '../../ui/pages/subpage/Subpage';
+import Subpage from '../../ui/pages/accounts/Subpage';
 
 import Compose from '../../ui/pages/compose/Compose'
-
+import Accounts from '../../ui/components/container/Accounts'
 
 export default (
     <React.Fragment>
+        <Accounts />
         <Route exact path="/" component={() => (<Main><Home /></Main>)} />
-        <Route exact path="/subpage" component={() => (<Main><Subpage /></Main>)} />
         <Route exact path="/compose" component={() => (<Secondary><Compose /></Secondary>)} />
     </React.Fragment>
 );
---
+++ imports/ui/components/container/Accounts.js
@@ -0,0 +1,84 @@
+import React from 'react'
+
+const Accounts = (props) => {
+    // const { posts } = props
+
+    
+
+    return (
+        <section>
+            <div className="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                <div className="modal-dialog modal-dialog-centered" role="document">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="exampleModalLongTitle">Modal title</h5>
+                            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+
+
+                        <div className="modal-body">
+                            <form>
+                                <div className="form-group">
+                                    <label htmlFor="exampleInputEmail1">Email address</label>
+                                    <input type="email" className="form-control" id="exampleInputEmail1"
+                                           aria-describedby="emailHelp" placeholder="Enter email" />
+                                        <small id="emailHelp" className="form-text text-muted">We'll never share your
+                                            email with anyone else.
+                                        </small>
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor="exampleInputPassword1">Password</label>
+                                    <input type="password" className="form-control" id="exampleInputPassword1"
+                                           placeholder="Password" />
+                                </div>
+                                <div className="form-check">
+                                    <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+                                        <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+                                </div>
+                                <button type="submit" className="btn btn-primary">Submit</button>
+                            </form>
+                        </div>
+
+
+
+
+                        <div className="modal-body">
+                            <form>
+                                <div className="form-group">
+                                    <label htmlFor="exampleInputEmail1">Email address</label>
+                                    <input type="email" className="form-control" id="exampleInputEmail1"
+                                           aria-describedby="emailHelp" placeholder="Enter email" />
+                                    <small id="emailHelp" className="form-text text-muted">We'll never share your
+                                        email with anyone else.
+                                    </small>
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor="exampleInputPassword1">Password</label>
+                                    <input type="password" className="form-control" id="exampleInputPassword1"
+                                           placeholder="Password" />
+                                </div>
+                                <div className="form-check">
+                                    <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+                                    <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+                                </div>
+                                <button type="submit" className="btn btn-primary">Submit</button>
+                            </form>
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+                            <button type="button" className="btn btn-primary">Save changes</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
+
+Accounts.propTypes = {
+
+};
+
+export default Accounts;
