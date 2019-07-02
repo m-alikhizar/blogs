b3355572667ba718e0649d418c91678748093cf5
Khizar Ali <khizar.ali.manzoor@gmail.com>
2019-07-02 08:33:24 +0500

    enabled at config for accounts.

--- imports/startup/server/index.js
+++ imports/startup/server/index.js
@@ -3,6 +3,7 @@ import './register-api'
 import './ssr-init'
 // import './accounts-config'
 
+T9n.setLanguage('en');
 
 //const T9n = (require('meteor-accounts-t9n')).T9n
 //T9n.setTracker(require('meteor/tracker'))
